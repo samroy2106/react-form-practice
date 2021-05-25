b5ea2bc42e0a4b4698e40e41f1e174ceb57e967f
Samarth Roy <samarthroy2106@gmail.com>
2021-05-25 20:49:10 +0530

    React form with CSS from CodePen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  name: '',
+  email: '',
+  phone_number: '',
+  address: '',
+  gender: '',
+  age: '',
+  maritial_status: ''
+}
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  onChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      [event.target.name]: value
+    })
+  }
+
+  onSubmit = (event) => {
+    //Typically, we would run a fetch POST here to hit the API endpoint and accept the values to the backend.
+    //However, since we do not have a backend, we display submitted values in an alert.
+    event.preventDefault();
+    alert('Name: ' + this.state.name
+          + '\nEmail: ' + this.state.email
+          + '\nPhone Number: ' + this.state.phone_number
+          + '\nAddress: ' + this.state.address
+          + '\nGender: ' + this.state.gender
+          + '\nAge: ' + this.state.age
+        );
+  }
+
+  render() {
+    return (
+      <div className="app-box">
+        <h2>Form Handling Practice</h2>
+        <form>
+          <div class='input-box'>
+            <input name='name' value={this.state.name} type='text' onChange={this.onChange} />
+            <label>Name</label>
+          </div>
+          <br />
+          <div class='input-box'>
+            <input name='email' value={this.state.email} type='text' onChange={this.onChange} />
+            <label>Email</label>
+          </div>
+          <br />
+          <div class='input-box'>
+            <input name='phone_number' value={this.state.phone_number} type='number' onChange={this.onChange} />
+            <label>Phone Number</label>
+          </div>
+          <br />
+          <div class='input-box'>
+            <input name='address' value={this.state.address} type='text' onChange={this.onChange} />
+            <label>Address</label>
+          </div>
+          <br />
+          <div class='input-box'>
+            <select name='gender' value={this.state.gender} onChange={this.onChange} >
+              <option value='Male'>Male</option>
+              <option value='Female'>Female</option>
+              <option value='Other'>Other</option>
+            </select>
+            <label>Gender</label>
+          </div>
+          <br />
+          <div class='input-box'>
+            <input name='age' value={this.state.age} type='number' onChange={this.onChange} />
+            <label>Age: </label>
+          </div>
+          <br />
+          <a href='#' onClick={this.onSubmit}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
